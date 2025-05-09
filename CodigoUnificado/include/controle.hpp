7747d4320bbee993546ef7c3b68fe5fac0fb9cf1
Rafael Gabriel <rafaeldzn2003@gmail.com>
2025-05-09 08:15:50 -0300

    Criando a classe para o controle da bomba

---
+++ CodigoUnificado/include/controle.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <Arduino.h>
+
+class ControleUmidade{
+
+    private:
+
+    int _sensorUmidade;
+    int _bombaAgua;
+    unsigned int _tempoIrrigacao;
+    int _tempoIntervaloLeitura;
+    int _umidadeMinima;
+    
+    public:
+    
+    //Variaveis auxiliares
+    bool bombaLigada = false;
+    bool intervaloLeitura = false;
+    unsigned long* tempoAtual;
+
+    ControleUmidade(int sensorUmidade, int bombaAgua, unsigned int tempoIrrigacao, int tempoIntervaloLeitura, int umidadeMinima, unsigned long* tempoAtual):
+        
+        _sensorUmidade(sensorUmidade),
+        _bombaAgua(bombaAgua),
+        _tempoIrrigacao(tempoIrrigacao), //Em segundos
+        _tempoIntervaloLeitura(tempoIntervaloLeitura), //Em segundos
+        _umidadeMinima(umidadeMinima), //Na escala de 0 a 3000 (fazer um map depois)
+        tempoAtual(tempoAtual){} //Em millisegundos
+
+    //Faz a configuracao inicial dos sensores e atuadores
+    void set(){
+
+        //Definindo os pinos
+        pinMode(_sensorUmidade, INPUT);
+        pinMode(_bombaAgua, OUTPUT);
+
+        //Definindo a saida da bomba para HIGH, pois ela liga em LOW
+        digitalWrite(_bombaAgua, HIGH);
+
+    }
+
+    void update(){
+
+        if ((!bombaLigada) && (intervaloLeitura) && analogRead(_sensorUmidade) >= _umidadeMinima){
+    
+            // Ativamos a bomba caso a umidade esteja menor que a umidade minima  
+            digitalWrite(_bombaAgua, LOW);
+            bombaLigada = true;
+            *tempoAtual = millis();
+        
+        }
+        
+        if (bombaLigada && (millis() - *tempoAtual >= _tempoIrrigacao)){
+        
+            // Desliga a bomba e inicia a contagem no intervalo de medicao
+            digitalWrite(_bombaAgua, HIGH);
+            bombaLigada = false;
+            intervaloLeitura = true;
+            *tempoAtual = millis();
+            
+        }
+        
+        if (intervaloLeitura && (millis() - *tempoAtual >= _tempoIntervaloLeitura*1000)){
+        
+            intervaloLeitura = false;
+        
+        }
+
+    }
+
+};
